e6a8e58f9e1aef2ff6c48c92685418e15c4b3ba3
Jose Manuel Lezcano Fresno <reymidas76@yahoo.es>
2022-09-19 09:55:10 +0200

    comment test obtain array of one course

--- 03-02-repository-test/internal/platform/server/handler/courses/get_all_test.go
+++ 03-02-repository-test/internal/platform/server/handler/courses/get_all_test.go
@@ -15,6 +15,12 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+type CourseResponse struct {
+	Id       string
+	Name     string
+	Duration string
+}
+
 func TestHandler_GetAll(t *testing.T) {
 
 	gin.SetMode(gin.TestMode)
@@ -48,4 +54,55 @@ func TestHandler_GetAll(t *testing.T) {
 		assert.Equal(t, []mooc.Course{}, response)
 
 	})
+
+	/*t.Run("obtain array of courses", func(t *testing.T) {
+
+		course, _ := mooc.NewCourse("8a1c5cdc-ba57-445a-994d-aa412d23723f", "Courses Complete", "123")
+		var someCourses = []mooc.Course{course}
+
+		fmt.Println(someCourses)
+
+		courseRepository := new(storagemocks.CourseRepository)
+		courseRepository.On("GetAll", mock.Anything).Return(someCourses, nil)
+
+		r := gin.New()
+		r.GET("/courses", GetHandler(courseRepository))
+
+		req, err := http.NewRequest(http.MethodGet, "/courses", nil)
+		//ultimo parametro
+		require.NoError(t, err)
+
+		rec := httptest.NewRecorder()
+		r.ServeHTTP(rec, req)
+
+		res := rec.Result()
+		defer res.Body.Close()
+
+		assert.Equal(t, http.StatusOK, res.StatusCode)
+
+		var response []mooc.Course
+		if err := json.NewDecoder(res.Body).Decode(&response); err != nil {
+			log.Fatalln(err)
+		}
+
+		assert.Equal(t, convertToCourseResponse(someCourses), response)
+	})*/
+}
+
+func convertToCourseResponse(courses []mooc.Course) []CourseResponse {
+	var response []CourseResponse
+
+	if len(courses) == 0 {
+		return response
+	}
+
+	for _, course := range courses {
+		response = append(response, CourseResponse{
+			Id:       course.ID().String(),
+			Name:     course.Name().String(),
+			Duration: course.Duration().String(),
+		})
+	}
+
+	return response
 }
